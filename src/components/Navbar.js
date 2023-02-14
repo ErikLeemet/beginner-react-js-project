72ec7baea0e0ad1bb3ac38d00e753615f7e4e1f6
Erik Leemet <Erik.Leemet@energia.ee>
2023-02-14 13:48:45 +0200

    new

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function NavBar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            TRVL
+          </Link>
+        </div>
+        <div className="menu-icon">
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default NavBar
